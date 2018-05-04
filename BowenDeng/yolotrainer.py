4f53f74cfc4591026b23482b078368144530f112
Bowen Deng <dbw15@mails.tsinghua.edu.cn>
2018-05-04 21:32:31 +0800

    update yolotrainer

--- BowenDeng/yolotrainer.py
+++ BowenDeng/yolotrainer.py
@@ -1,7 +1,8 @@
-
-
+import random
+from PIL import Image
 import torch.optim as optim
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+import torchvision.transforms as transforms
 from DatasetGenerator import DatasetGenerator
 
 
@@ -11,11 +12,11 @@ class YoloTrainer():
 
     def train(self, pathDirData, pathFileTrain, pathFileVal, trBatchSize, trMaxEpoch):
 
-
         model = torch.nn.DataParallel(model).cuda()
 
         # -------------------- DATA ARGUMENT??
         transformList = []
+        transformList.append(transforms.ToTensor())
         transformSequence = transforms.Compose(transformList)
 
         # -------------------- SETTINGS: DATASET BUILDERS
@@ -31,19 +32,18 @@ class YoloTrainer():
 
         # -------------------- SETTINGS: OPTIMIZER & SCHEDULER
         # need to know what is used, to be done
-        optimizer = optim.Adam(model.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-5)
-        # scheduler = ReduceLROnPlateau(optimizer, factor=0.1, patience=1, mode='min')
+        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0005)
+        scheduler = optim.MultiStepLR(optimizer, milestones=[60, 90], gamma=0.1)
 
         # -------------------- SETTINGS: LOSS
         # need to know what is used, to be done
         # loss =
 
-
         # ---- TRAIN THE NETWORK
-
+        #MaxEpoch=?
         lossMIN = 10000
         for epoch in range(0, MaxEpoch):
-
+            scheduler.step()
             self.epochTrain(model, dataLoaderTrain, optimizer, loss)
             lossVal, losstensor = self.epochVal(model, dataLoaderVal, optimizer, scheduler, trMaxEpoch, loss)
 
@@ -106,3 +106,66 @@ class YoloTrainer():
         losstensorMean = losstensorMean / lossValNorm
 
         return outLoss, losstensorMean
+
+
+def distort_image(im, hue, sat, val):
+    im = im.convert('HSV')
+    cs = list(im.split())
+    cs[1] = cs[1].point(lambda i: i * sat)
+    cs[2] = cs[2].point(lambda i: i * val)
+
+    def change_hue(x):
+        x += hue * 255
+        if x > 255:
+            x -= 255
+        if x < 0:
+            x += 255
+        return x
+
+    cs[0] = cs[0].point(change_hue)
+    im = Image.merge(im.mode, tuple(cs))
+
+    im = im.convert('RGB')
+    # constrain_image(im)
+    return im
+
+
+def rand_scale(s):
+    scale = random.uniform(1, s)
+    if (random.randint(1, 10000) % 2):
+        return scale
+    return 1. / scale
+
+
+def random_distort_image(im, hue, saturation, exposure):
+    dhue = random.uniform(-hue, hue)
+    dsat = rand_scale(saturation)
+    dexp = rand_scale(exposure)
+    res = distort_image(im, dhue, dsat, dexp)
+    return res
+
+
+def data_augmentation(img, shape, jitter, hue, saturation, exposure):
+    oh = img.height
+    ow = img.width
+
+    dw = int(ow * jitter)
+    dh = int(oh * jitter)
+
+    pleft = random.randint(-dw, dw)
+    pright = random.randint(-dw, dw)
+    ptop = random.randint(-dh, dh)
+    pbot = random.randint(-dh, dh)
+
+    swidth = ow - pleft - pright
+    sheight = oh - ptop - pbot
+
+    flip = random.randint(1, 10000) % 2
+    cropped = img.crop((pleft, ptop, pleft + swidth - 1, ptop + sheight - 1))
+
+    sized = cropped.resize(shape)
+
+    if flip:
+        sized = sized.transpose(Image.FLIP_LEFT_RIGHT)
+    img = random_distort_image(sized, hue, saturation, exposure)
+    return img
