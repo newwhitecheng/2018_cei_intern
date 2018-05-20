7145c35c94ff35aaa839a7970f00d7152d046245
Bowen Deng <dbw15@mails.tsinghua.edu.cn>
2018-05-20 22:34:01 +0800

    update loss

--- BowenDeng/Yolotrainer.py
+++ BowenDeng/Yolotrainer.py
@@ -1,12 +1,69 @@
 import random
 from PIL import Image
 import torch.optim as optim
-
+import torch
+import torch.nn as nn
 import torchvision.transforms as transforms
-from DatasetGenerator import DatasetGenerator
-
-
+from Datagenerator import DatasetGenerator
+from torch.utils.data import DataLoader
+import numpy as np
 # --------------------------------------------------------------------------------
+class loss(nn.Module):
+    """
+    objectness score represents the probability an object is contained inside a bb.
+    class confidence represent the probabilities of the dectected object belonging to
+    particular class.
+    define the confidence as Pr(obj) * IOU, if no obj exists, the confidence score = 0,
+    otherwise it should equal the IOU
+    C conditional class probabilitie s Pr(Classi|Obj). These probabilities are conditioned on
+    the grid cell containing an obj.
+    At test time, multiply the conditional class probabilities and the individual box confidence
+    class probabilities * confidence = Pr(Classi) * IOU
+    Increase bb coordinate loss by a factor of 5 and decrease the confidence loss for boxes that
+    don't contain objs by a factor of 0.5
+    :return:
+    """
+    def __init__(self):
+        super(loss, self).__init__()
+
+    def forward(self, prediction, label, lamda_obj=5, lamda_noobj=0.5):
+        indexer = torch.from_numpy(np.argsort(prediction.cpu.numpy()[:,0]))
+        prediction = prediction[indexer]
+        row_label = label[0]
+        # 假设label_name是class index
+        label_name = []
+        batch_id = -1
+        loss = 0
+
+        for row in range(prediction.size(0)):
+            if batch_id != prediction[row][0]:
+                batch_id = prediction[row][0]
+                label_name = [(i, row_label[i]) for i in range(0, len(row_label), 5)]
+            flag = 0
+            for item in label_name:
+                if prediction[row] == item[1]:
+                    begin = item[0]
+                    x_min, y_min = row_label[begin + 1], row_label[begin + 3]
+                    x_max, y_max = row_label[begin + 2], row_label[begin + 4]
+                    delta_x = (x_min + x_max) / 2 - (prediction[row][1] + prediction[row][3]) / 2
+                    delta_y = (y_min + y_max) / 2 - (prediction[row][2] + prediction[row][4]) / 2
+                    delta_h = torch.sqrt(y_max - y_min) - torch.sqrt(prediction[row][4] + prediction[row][2])
+                    delta_w = torch.sqrt(x_max - x_min) - torch.sqrt(prediction[row][3] + prediction[row][1])
+                    loss += torch.pow(delta_x, 2) + torch.pow(delta_y, 2)
+                    loss += torch.pow(delta_w, 2) + torch.pow(delta_h, 2)
+                    loss += torch.pow(prediction[row][5] - 1, 2)
+                    loss += torch.pow(prediction[row][6] - 1, 2)
+                    loss += torch.pow(prediction[row][1] - row_label)
+                    flag += 1
+                    break
+            if flag != 1:
+                loss += 2
+        return loss
+
+
+
+
+
 
 class YoloTrainer():
 
@@ -40,7 +97,7 @@ class YoloTrainer():
         # loss =
 
         # ---- TRAIN THE NETWORK
-        #MaxEpoch=?
+        MaxEpoch=1000
         lossMIN = 10000
         for epoch in range(0, MaxEpoch):
             scheduler.step()
