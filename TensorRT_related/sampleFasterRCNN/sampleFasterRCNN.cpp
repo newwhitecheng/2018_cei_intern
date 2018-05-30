772bca5a6ba8c585245b4ea317980d0a57b555ed
fairchildfzc <fairchildfzc@hotmail.com>
2018-05-30 08:42:56 +0800

    add opencv in code

--- TensorRT_related/sampleFasterRCNN/sampleFasterRCNN.cpp
+++ TensorRT_related/sampleFasterRCNN/sampleFasterRCNN.cpp
@@ -12,11 +12,19 @@
 #include <memory>
 #include <cstring>
 #include <algorithm>
-
+#include <string.h>  
+#include <vector>
+#include <unistd.h>  
+#include <dirent.h>       
+#include <stdio.h>           
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"   
 #include "NvCaffeParser.h"
 #include "NvInferPlugin.h"
 #include "common.h"
-
+using namespace std;
+using namespace cv;
 static Logger gLogger;
 using namespace nvinfer1;
 using namespace nvcaffeparser1;
@@ -24,13 +32,13 @@ using namespace plugin;
 
 // stuff we know about the network and the caffe input/output blobs
 static const int INPUT_C = 3;
-static const int INPUT_H = 375;
-static const int INPUT_W = 500;
+static const int INPUT_H = 224;
+static const int INPUT_W = 224;
 static const int IM_INFO_SIZE = 3;
-static const int OUTPUT_CLS_SIZE = 21;
+static const int OUTPUT_CLS_SIZE = 201;
 static const int OUTPUT_BBOX_SIZE = OUTPUT_CLS_SIZE * 4;
 
-const std::string CLASSES[OUTPUT_CLS_SIZE]{ "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor" };
+const std::string CLASSES[OUTPUT_CLS_SIZE]{ "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor" };
 
 const char* INPUT_BLOB_NAME0 = "data";
 const char* INPUT_BLOB_NAME1 = "im_info";
@@ -51,6 +59,7 @@ const float minBoxSize = 16;
 const float spatialScale = 0.0625f;
 const float anchorsRatios[anchorsRatioCount] = { 0.5f, 1.0f, 2.0f };
 const float anchorsScales[anchorsScaleCount] = { 8.0f, 16.0f, 32.0f };
+const std::string filepath = "/data/faster-rcnn/Images_2018_ppm_reshape/";
 
 struct PPM
 {
@@ -64,9 +73,30 @@ struct BBox
 	float x1, y1, x2, y2;
 };
 
+void jpg2ppm(const std::string& filename, PPM& ppm)
+{
+ ppm.fileName = filename;
+ cv::Mat jpg;
+ std::string imageinput = filepath+filename;
+ cv::Mat rawjpg = cv::imread(imageinput, -1);
+ ppm.h = rawjpg.rows;
+ ppm.w = rawjpg.cols;
+ cv::resize(rawjpg, jpg, cv::Size(224, 224));
+ for(int j = 0; j<jpg.rows; j++){
+  uint8_t *data = jpg.ptr<uchar>(j);
+  for(int i = 0; i<jpg.cols ; i++)
+  {
+   ppm.buffer[(j*jpg.rows+i)*3] = data[3*i+2];
+   ppm.buffer[(j*jpg.rows+i)*3+1] = data[3*i+1];
+   ppm.buffer[(j*jpg.rows+i)*3+2] = data[3*i+0];
+  }
+ }
+ return;
+}
+
 std::string locateFile(const std::string& input)
 {
-    std::vector<std::string> dirs{"data/samples/faster-rcnn/", "data/faster-rcnn/"};
+    std::vector<std::string> dirs{"data/samples/faster-rcnn/", "data/faster-rcnn/","/home/nvidia/workspace/Images_2018_ppm_reshape/","/home/nvidia/workspace/Images_2018/","/home/nvidia/workspace/Images_2018_reshape/"};
     return locateFile(input, dirs);
 }
 
@@ -110,7 +140,36 @@ void writePPMFileWithBBox(const std::string& filename, PPM& ppm, const BBox& bbo
 	}
 	outfile.write(reinterpret_cast<char*>(ppm.buffer), ppm.w * ppm.h * 3);
 }
-
+//Read all the pics in one folder
+std::vector<std::string> getFiles(std::string cate_dir)  
+{  
+    std::vector<std::string> files;   
+    DIR *dir;  
+    struct dirent *ptr;  
+    //char base[1000];  
+   
+    if ((dir=opendir(cate_dir.c_str())) == NULL)  
+        {  
+        perror("Open dir error...");  
+                exit(1);  
+        }  
+   
+    while ((ptr=readdir(dir)) != NULL)  
+    {  
+        if(strcmp(ptr->d_name,".")==0 || strcmp(ptr->d_name,"..")==0)    ///current dir OR parrent dir  
+                continue;  
+        else if(ptr->d_type == 8)    ///file  
+            files.push_back(ptr->d_name);  
+        else if(ptr->d_type == 10)    ///link file  
+            continue;  
+        else if(ptr->d_type == 4)    ///dir  
+        {  
+            files.push_back(ptr->d_name);  
+        }  
+    }  
+    closedir(dir);  
+    return files;  
+}  
 void caffeToGIEModel(const std::string& deployFile,			// name for caffe prototxt
 	const std::string& modelFile,			// name for model 
 	const std::vector<std::string>& outputs,		// network outputs
@@ -130,7 +189,7 @@ void caffeToGIEModel(const std::string& deployFile,			// name for caffe prototxt
 	const IBlobNameToTensor* blobNameToTensor = parser->parse(locateFile(deployFile).c_str(),
 		locateFile(modelFile).c_str(),
 		*network,
-		DataType::kFLOAT);
+		nvinfer1::DataType::kFLOAT);
 	std::cout << "End parsing model..." << std::endl;
 	// specify which tensors are outputs
 	for (auto& s : outputs)
@@ -175,7 +234,7 @@ void doInference(IExecutionContext& context, float* inputData, float* inputImInf
 
 
 	// create GPU buffers and a stream
-	CHECK(cudaMalloc(&buffers[inputIndex0], batchSize * INPUT_C * INPUT_H * INPUT_W * sizeof(float)));   // data
+	CHECK(cudaMalloc(&buffers[inputIndex0], batchSize * INPUT_C * INPUT_H * INPUT_W * sizeof(float)));   // affeparser1::data
 	CHECK(cudaMalloc(&buffers[inputIndex1], batchSize * IM_INFO_SIZE * sizeof(float)));                  // im_info
 	CHECK(cudaMalloc(&buffers[outputIndex0], batchSize * nmsMaxOut * OUTPUT_BBOX_SIZE * sizeof(float))); // bbox_pred
 	CHECK(cudaMalloc(&buffers[outputIndex1], batchSize * nmsMaxOut * OUTPUT_CLS_SIZE * sizeof(float)));  // cls_prob
@@ -441,122 +500,128 @@ int main(int argc, char** argv)
 	PluginFactory pluginFactory;
 	IHostMemory *gieModelStream{ nullptr };
 	// batch size
-	const int N = 2;
+	const int N = 1;
+        const int M = 200;
 	caffeToGIEModel("faster_rcnn_test_iplugin.prototxt",
-		"VGG16_faster_rcnn_final.caffemodel",
+		"vgg16_faster_rcnn_iter_80000.caffemodel",
 		std::vector < std::string > { OUTPUT_BLOB_NAME0, OUTPUT_BLOB_NAME1, OUTPUT_BLOB_NAME2 },
 		N, &pluginFactory, &gieModelStream);
 
 	pluginFactory.destroyPlugin();
 	// read a random sample image
-	srand(unsigned(time(nullptr)));
+	//srand(unsigned(time(nullptr)));
 	// available images 
-	std::vector<std::string> imageList = { "000456.ppm",  "000542.ppm",  "001150.ppm", "001763.ppm", "004545.ppm" };
-	std::vector<PPM> ppms(N);
-
-	float imInfo[N * 3]; // input im_info	
-	std::random_shuffle(imageList.begin(), imageList.end(), [](int i) {return rand() % i; });
-	assert(ppms.size() <= imageList.size());
-	for (int i = 0; i < N; ++i)
-	{
-		readPPMFile(imageList[i], ppms[i]);
-		imInfo[i * 3] = float(ppms[i].h);   // number of rows
-		imInfo[i * 3 + 1] = float(ppms[i].w); // number of columns
-		imInfo[i * 3 + 2] = 1;         // image scale
-	}
-
-	float* data = new float[N*INPUT_C*INPUT_H*INPUT_W];
-	// pixel mean used by the Faster R-CNN's author
-	float pixelMean[3]{ 102.9801f, 115.9465f, 122.7717f }; // also in BGR order
-	for (int i = 0, volImg = INPUT_C*INPUT_H*INPUT_W; i < N; ++i)
-	{
-		for (int c = 0; c < INPUT_C; ++c)
-		{
-			// the color image to input should be in BGR order
-			for (unsigned j = 0, volChl = INPUT_H*INPUT_W; j < volChl; ++j)
-				data[i*volImg + c*volChl + j] = float(ppms[i].buffer[j*INPUT_C + 2 - c]) - pixelMean[c];
-		}
-	}
-
-	// deserialize the engine 
+        //std::vector<std::string> imageList = { "000456.ppm",  "000542.ppm",  "001150.ppm", "001763.ppm", "004545.ppm" };
+	//std::vector<std::string> imageList = {"LPIRC2018_test_00000001.ppm","LPIRC2018_test_00000002.ppm","LPIRC2018_test_00000003.ppm","LPIRC2018_test_00000004.ppm","LPIRC2018_test_00000005.ppm"};
+//	std::vector<std::string> imageList=getFiles("/home/nvidia/workspace/Images_2018");
+        std::vector<std::string> imageList=getFiles("/home/nvidia/workspace/Images_2018_ppm_reshape");
+        std::vector<PPM> ppms(N);
+        // deserialize the engine 
 	IRuntime* runtime = createInferRuntime(gLogger);
 	ICudaEngine* engine = runtime->deserializeCudaEngine(gieModelStream->data(), gieModelStream->size(), &pluginFactory);
 
 	IExecutionContext *context = engine->createExecutionContext();
 
-
-	// host memory for outputs 
-	float* rois = new float[N * nmsMaxOut * 4];
-	float* bboxPreds = new float[N * nmsMaxOut * OUTPUT_BBOX_SIZE];
-	float* clsProbs = new float[N * nmsMaxOut * OUTPUT_CLS_SIZE];
-
-	// predicted bounding boxes
-	float* predBBoxes = new float[N * nmsMaxOut * OUTPUT_BBOX_SIZE];
-
-	// run inference
-	doInference(*context, data, imInfo, bboxPreds, clsProbs, rois, N);
-
-	// destroy the engine
-	context->destroy();
-	engine->destroy();
-	runtime->destroy();
-	pluginFactory.destroyPlugin();
-
-	// unscale back to raw image space
-	for (int i = 0; i < N; ++i)
-	{
-		float * rois_offset = rois + i * nmsMaxOut * 4;
-		for (int j = 0; j < nmsMaxOut * 4 && imInfo[i * 3 + 2] != 1; ++j)
-			rois_offset[j] /= imInfo[i * 3 + 2];
-	}
-
-	bboxTransformInvAndClip(rois, bboxPreds, predBBoxes, imInfo, N, nmsMaxOut, OUTPUT_CLS_SIZE);
-
-	const float nms_threshold = 0.3f;
-	const float score_threshold = 0.8f;
-
-	for (int i = 0; i < N; ++i)
-	{
-		float *bbox = predBBoxes + i * nmsMaxOut * OUTPUT_BBOX_SIZE;
-		float *scores = clsProbs + i * nmsMaxOut * OUTPUT_CLS_SIZE;
-		for (int c = 1; c < OUTPUT_CLS_SIZE; ++c) // skip the background
-		{
-			std::vector<std::pair<float, int> > score_index;
-			for (int r = 0; r < nmsMaxOut; ++r)
-			{
-				if (scores[r*OUTPUT_CLS_SIZE + c] > score_threshold)
-				{
-					score_index.push_back(std::make_pair(scores[r*OUTPUT_CLS_SIZE + c], r));
-					std::stable_sort(score_index.begin(), score_index.end(),
-						[](const std::pair<float, int>& pair1,
-							const std::pair<float, int>& pair2) {
-						return pair1.first > pair2.first;
-					});
-				}
-			}
-
-			// apply NMS algorithm
-			std::vector<int> indices = nms(score_index, bbox, c, OUTPUT_CLS_SIZE, nms_threshold);
-			// Show results
-			for (unsigned k = 0; k < indices.size(); ++k)
-			{
-				int idx = indices[k];
-				std::string storeName = CLASSES[c] + "-" + std::to_string(scores[idx*OUTPUT_CLS_SIZE + c]) + ".ppm";
-				/*std::cout << "Detected " << CLASSES[c] << " in " << ppms[i].fileName << " with confidence " << scores[idx*OUTPUT_CLS_SIZE + c] * 100.0f << "% "
-					<< " (Result stored in " << storeName << ")." << std::endl;*/
-
-				BBox b{ bbox[idx*OUTPUT_BBOX_SIZE + c * 4], bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 1], bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 2], bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 3] };
-                                std::cout  << ppms[i].fileName << " " << c <<  " " <<  scores[idx*OUTPUT_CLS_SIZE + c]  <<  " " << b.x1  << " " <<  b.y1 << " " <<  b.x2 << " " <<  b.y2 << std::endl;
-				writePPMFileWithBBox(storeName, ppms[i], b);
-			}
-		}
-	}
+	float imInfo[N * 3]; // input im_info	
+	//std::random_shuffle(imageList.begin(), imageList.end(), [](int i) {return rand() % i; });
+	assert(ppms.size() <= imageList.size());
+	for(int pn = 0; pn<M; ++pn)
+        {
+                for (int i = 0; i < N; ++i)
+	        {
+		        readPPMFile(imageList[i+pn], ppms[i]);
+		        imInfo[i * 3] = float(ppms[i].h);   // number of rows
+		        imInfo[i * 3 + 1] = float(ppms[i].w); // number of columns
+		        imInfo[i * 3 + 2] = 1;         // image scale
+	        }
+                
+	        float* data = new float[N*INPUT_C*INPUT_H*INPUT_W];
+	        // pixel mean used by the Faster R-CNN's author
+	        float pixelMean[3]{ 102.9801f, 115.9465f, 122.7717f }; // also in BGR order
+	        for (int i = 0, volImg = INPUT_C*INPUT_H*INPUT_W; i < N; ++i)
+	        {
+		        for (int c = 0; c < INPUT_C; ++c)
+		        {
+			        // the color image to input should be in BGR order
+			        for (unsigned j = 0, volChl = INPUT_H*INPUT_W; j < volChl; ++j)
+			        	data[i*volImg + c*volChl + j] = float(ppms[i].buffer[j*INPUT_C + 2 - c]) - pixelMean[c];
+		        }
+	        }
 
 
-	delete[] data;
+	        // host memory for outputs 
+	        float* rois = new float[N * nmsMaxOut * 4];
+	        float* bboxPreds = new float[N * nmsMaxOut * OUTPUT_BBOX_SIZE];
+        	float* clsProbs = new float[N * nmsMaxOut * OUTPUT_CLS_SIZE];
+
+        	// predicted bounding boxes
+        	float* predBBoxes = new float[N * nmsMaxOut * OUTPUT_BBOX_SIZE];
+
+        	// run inference
+        	doInference(*context, data, imInfo, bboxPreds, clsProbs, rois, N);
+
+	
+
+        	// unscale back to raw image space
+        	for (int i = 0; i < N; ++i)
+        	{
+        		float * rois_offset = rois + i * nmsMaxOut * 4;
+        		for (int j = 0; j < nmsMaxOut * 4 && imInfo[i * 3 + 2] != 1; ++j)
+        			rois_offset[j] /= imInfo[i * 3 + 2];
+        	}
+        
+        	bboxTransformInvAndClip(rois, bboxPreds, predBBoxes, imInfo, N, nmsMaxOut, OUTPUT_CLS_SIZE);
+
+	        const float nms_threshold = 0.3f;
+	        const float score_threshold = 0.8f;
+
+	        for (int i = 0; i < N; ++i)
+	        {
+		        float *bbox = predBBoxes + i * nmsMaxOut * OUTPUT_BBOX_SIZE;
+		        float *scores = clsProbs + i * nmsMaxOut * OUTPUT_CLS_SIZE;
+		        for (int c = 1; c < OUTPUT_CLS_SIZE; ++c) // skip the background
+		        {
+		        	std::vector<std::pair<float, int> > score_index;
+		        	for (int r = 0; r < nmsMaxOut; ++r)
+		        	{
+			        	if (scores[r*OUTPUT_CLS_SIZE + c] > score_threshold)
+				        {
+				        	score_index.push_back(std::make_pair(scores[r*OUTPUT_CLS_SIZE + c], r));
+				        	std::stable_sort(score_index.begin(), score_index.end(),
+				        		[](const std::pair<float, int>& pair1,
+				        			const std::pair<float, int>& pair2) {
+				        		return pair1.first > pair2.first;
+				        	});
+				        }
+			        }
+
+			        // apply NMS algorithm
+			        std::vector<int> indices = nms(score_index, bbox, c, OUTPUT_CLS_SIZE, nms_threshold);
+			        // Show results
+			        for (unsigned k = 0; k < indices.size(); ++k)
+			        {
+				        int idx = indices[k];
+				        std::string storeName = CLASSES[c] + "-" + std::to_string(scores[idx*OUTPUT_CLS_SIZE + c]) + ".ppm";
+				        //std::cout << "Detected " << CLASSES[c] << " in " << ppms[i].fileName << " with confidence " << scores[idx*OUTPUT_CLS_SIZE + c] * 100.0f << "% "
+					//<< " (Result stored in " << storeName << ")." << std::endl;
+
+				        BBox b{ bbox[idx*OUTPUT_BBOX_SIZE + c * 4], bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 1], bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 2], bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 3] };
+                                    std::cout  << ppms[i].fileName << " " << c <<  " " <<  scores[idx*OUTPUT_CLS_SIZE + c]  <<  " " << b.x1  << " " <<  b.y1 << " " <<  b.x2 << " " <<  b.y2 << std::endl;
+				        //writePPMFileWithBBox(storeName, ppms[i], b);
+
+			        }
+		        }
+	        }
+        }
+
+	/*delete[] data;
 	delete[] rois;
 	delete[] bboxPreds;
 	delete[] clsProbs;
-	delete[] predBBoxes;
+	delete[] predBBoxes;*/
+        context->destroy();
+	engine->destroy();
+	runtime->destroy();
+	pluginFactory.destroyPlugin();
 	return 0;
 }
