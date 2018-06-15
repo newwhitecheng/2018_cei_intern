3220985dac21600604e5b00d0eb70e0ea4c3b5d2
newwhitecheng <newwhitecheng@gmail.com>
2018-06-15 15:20:39 -0400

    remove unused py-rfcn

--- py-R-FCN-imagenet/tools/start_multi.sh
+++
@@ -1,1 +0,0 @@
-python caffe_multi.py -w 192.168.0.107 --port 5000 --user user3 --pass pass3 
--- py-R-FCN-imagenet/tools/start_multi_backup.sh
+++
@@ -1,1 +0,0 @@
-python caffe_multi.py -w 192.168.1.2 --port 8000 --user user_330 --pass pass@#2249 1 >multi.log  2>&1 &
