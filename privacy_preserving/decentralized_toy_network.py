da655fb30cdb48ff04ec6bae3f9dd77d759b5fa2
patricks-lab <patricks-lab@users.noreply.github.com>
2018-07-09 10:29:33 -0700

    Working version of decentralized (non-privacy) model; need to speedup code

--- privacy_preserving/decentralized_toy_network.py
+++ privacy_preserving/decentralized_toy_network.py
@@ -1,35 +1,27 @@
-
-#Toy example of basic Four-node Decentralized Neural Network. Currently in progress as of 7/5/18 (does not work YET)
-#by Patrick
-
-'''TBD: 
-1. Gradient limiting factor Theta_U,Theta_D (for upload/download); 
-2. Gradient clipping; 
-3. (Differential privacy) Laplace Noise maybe?
-'''
-
-#Helper function to swap identities of gradients from one to the other. 
-#I.e. if g_W1[2] belongs to node 2 but we want to copy this gradient over to node 3 using apply_gradient, 
-#we need to swapIdentity(g_W1[2], W1[3]) -> gradient for node 2, but once we send to apply_gradient,
-#it sees it as gradient for node 3.
-def swapIdentity(gradient, newIdentity):
-    newGrad = gradient
-    newGrad[1] = newIdentity
-    return newGrad
-
-#-----------main code----------------
 import tensorflow as tf
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True) #download MNIST data (train, validate, test)
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.log_device_placement = True
+
+#Helper function to swap identities of gradients from one to the other.
+def swapIdentity(gradient, newIdentity):
+    return (gradient[0], newIdentity) #return new tuple with the identity replaced
+
+def shp(x):
+    return np.shape(x) 
 
 tf.reset_default_graph()
 
-#Hyperparameters for future use:
+#-----------------------Initialization---------------------------
+#Hyperparameters:
 theta_D = 1 #download rate
 theta_U = 0.01 #Upload rate
-#epsilon?
 
-sz = 4 #number of Computational nodes
+sz = 4 # size of network
 
 # Variables for our  4- way network
 W1 = [tf.Variable(tf.truncated_normal([784, 500], stddev=0.1)) for i in range(sz)]
@@ -39,20 +31,36 @@ b2 = [tf.Variable(tf.zeros([100]))                             for i in range(sz
 W3 = [tf.Variable(tf.truncated_normal([100, 10], stddev=0.1))  for i in range(sz)]
 b3 = [tf.Variable(tf.zeros([10]))                              for i in range(sz)]
 
+#Temporary gradient banks for communication between nodes (noise may be added in future)
+g_W1 = [None for i in range(sz)]
+g_W2 = [None for i in range(sz)]
+g_W3 = [None for i in range(sz)]
+g_b1 = [None for i in range(sz)]
+g_b2 = [None for i in range(sz)]
+g_b3 = [None for i in range(sz)]
 
-#Temporary gradient banks for privacy (noise may be added in future)
-g_W1 = g_W2 = g_W3 = [None for i in range(sz)]
-g_b1 = g_b2 = g_b3 = [None for i in range(sz)]
 
-grad_W1 = grad_W2 = grad_W3 = grad_b1 = grad_b2 = grad_b3 = [[None for i in range(sz)] for i in range(sz)]
+grad_W1 = [[None for i in range(sz)] for i in range(sz)]
+grad_W2 = [[None for i in range(sz)] for i in range(sz)]
+grad_W3 = [[None for i in range(sz)] for i in range(sz)]
+grad_b1 = [[None for i in range(sz)] for i in range(sz)]
+grad_b2 = [[None for i in range(sz)] for i in range(sz)]
+grad_b3 = [[None for i in range(sz)] for i in range(sz)]
 
-#hidden layers, scores, etc.
-h1 = h2= [None for i in range(sz)]
-scores = loss= [None for i in range(sz)]
+
+#hidden layers, scores, etc. *Note there is somehow a problem with saying scores = loss = [None for i in range(sz)] so I put them in seperate lines
+h1 = [None for i in range(sz)]
+h2 = [None for i in range(sz)]
+
+scores = [None for i in range(sz)]
+loss = [None for i in range(sz)]
+
+#-------------------------Define Decentralized Neural Network Structure-----------------
 
 # Placeholders
 X = [tf.placeholder(tf.float32, [None, 784])  for i in range(sz)]
 y = [tf.placeholder(tf.float32, [None, 10])    for i in range(sz)]
+
 
 # Deep Neural nets for 4 channels w/ loss function
 for i in range(sz):
@@ -68,12 +76,12 @@ for i in range(sz):
 
 opt = tf.train.GradientDescentOptimizer(0.05)
 
-correct_predictions = [None for i in range(sz)]
-accuracy = [None for i in range(sz)]
+correct_predictions = [0.0 for i in range(sz)]
+accuracy = [0.0 for i in range(sz)]
 
 # Accuracy indicator for 4 channels
 for i in range(sz):
-    correct_predictions[i] = tf.equal(tf.argmax(scores[i], axis=1), tf.argmax(y[i], axis=1))
+    correct_predictions[i] = tf.equal(tf.argmax(scores[i], axis=1), tf.argmax(y[i], axis=1)) #something wrong with the axis dimension :-( !!
     accuracy[i] = tf.reduce_mean(tf.cast(correct_predictions[i], tf.float32))
 
 # Initialization of all decleared Tensors (above) in Tensorflow
@@ -83,55 +91,57 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init) #initalize all variables
 
-train_acc=[0 for i in range(sz)]
-train_loss=[0 for i in range(sz)]
+#Gradient "bank"
 
+train_acc=[0.0 for i in range(sz)]
+train_loss=[0.0 for i in range(sz)]
 
+#------------------------------Begin Training------------------------------
 
-#for our case, WLOG (Without Loss of Generality) let's assume there are FOUR DECENTRALIZED NODES
-#WLOG let's call current node A, and neighbor computational nodes B,C,and D.
+#Split data into 4-ths. (750,750,750,750)
 
+for i in range(3000 // sz):
+    
+    for cnode in range(sz):
+ 
+        #----------download neighboring nodes' gradients (in this case, all (sz choose 2) or sz(sz-1)/2 nodes )------------
+        for nei in range(sz):
+            #Don't double train same node
+            if(nei == cnode):
+                continue
 
-#Split data into 4-ths. WLOG 4 nodes: (750,750,750,750) epochs for A,B,C, and D
-for cnode in range(sz):
-    for i in range(3000 // sz):
+            #Skip during 1st run of code
+            if(g_W1[nei] == None): 
+                continue
 
-        if(g_W1[nei] != None and g_W2[nei] != None and g_W3[nei] == None):   #*****!!!Skip gradient download if no data in them initially!!!**** 
-            #****repurpose gradients of B, C, D to go to A***
-            for nei in range(sz):
-                if(nei == cnode): #i.e. if it already is A, ignore
-                    continue
-
-
-                #Swap identities of neighbors' gradients with current node
-                #assert(g_W1[nei][1] == W1[nei])
-
-                grad_W1[nei][cnode] = swapIdentity(g_W1[nei], W1[cnode])
-                grad_W2[nei][cnode] = swapIdentity(g_W2[nei], W2[cnode])
-                grad_W3[nei][cnode] = swapIdentity(g_W3[nei], W3[cnode])
-                grad_b1[nei][cnode] = swapIdentity(g_b1[nei], b1[cnode])
-                grad_b2[nei][cnode] = swapIdentity(g_b2[nei], b2[cnode])
-                grad_b3[nei][cnode] = swapIdentity(g_b3[nei], b3[cnode])
-
-            #***Download gradients from nodes B,C,and D and use them to affect A's gradient.***
-            for nei in range(sz):
-                if(nei == cnode):
-                    continue      
-                opt.apply_gradients([grad_W1[nei][cnode], grad_W2[nei][cnode], grad_W3[nei][cnode], grad_b1[nei][cnode], grad_b2[nei][cnode], grad_b3[nei][cnode]])
+            grad_W1[nei][cnode] = swapIdentity(g_W1[nei], W1[cnode])
+            grad_W2[nei][cnode] = swapIdentity(g_W2[nei], W2[cnode])
+            grad_W3[nei][cnode] = swapIdentity(g_W3[nei], W3[cnode])
+            grad_b1[nei][cnode] = swapIdentity(g_b1[nei], b1[cnode])
+            grad_b2[nei][cnode] = swapIdentity(g_b2[nei], b2[cnode])
+            grad_b3[nei][cnode] = swapIdentity(g_b3[nei], b3[cnode])
+            
+            opt.apply_gradients([grad_W1[nei][cnode], grad_W2[nei][cnode], grad_W3[nei][cnode], 
+                                 grad_b1[nei][cnode], grad_b2[nei][cnode], grad_b3[nei][cnode]])
+        #--------------------------------------------------------------
         
-        #***Take minibatch of 50, compute gradients w.r.t weights***
-        x_batch, y_batch = mnist.train.next_batch(50)
-        train_loss[cnode], train_acc[cnode] = sess.run([loss[cnode], accuracy[cnode]], feed_dict={X[cnode]: x_batch, y[cnode]: y_batch})
+        
+        #---------download minibatch of 50 & get gradients-----------------------
+        X_batch, y_batch = mnist.train.next_batch(50)
+        
         if(i % 100 == 0):
-            print("accuracy for Node: ", cnode,  train_acc[cnode])
+            train_acc[cnode] = sess.run(accuracy[cnode], feed_dict={X[cnode]: X_batch, y[cnode]: y_batch})
+            print("accuracy for Node: ", cnode,  "  is: ",train_acc[cnode],  "  at epoch: ", i)
 
         #upload trained weights to neighbors B, C
-        g_W1[cnode], g_W2[cnode], g_W3[cnode], g_b1[cnode], g_b2[cnode], g_b3[cnode] = compute_gradients(loss=loss[cnode], var_list=[W1[cnode], W2[cnode], W3[cnode], b1[cnode], b2[cnode], b3[cnode]])
+        g_W1[cnode], g_W2[cnode], g_W3[cnode], 
+        g_b1[cnode], g_b2[cnode], g_b3[cnode] = opt.compute_gradients(loss=loss[cnode], 
+                                                                      var_list=[W1[cnode], W2[cnode], W3[cnode],
+                                                                                b1[cnode], b2[cnode], b3[cnode]])
+        
+        sess.run(train_one_step[cnode], feed_dict={X[cnode]: X_batch, y[cnode]: y_batch})
+        #---------------------------------------------------------------------
 
-        #set some weights to Zero (for privacy); keeping largest theta_U% of weights
-        #TBD LATER
-
-
-#Print out your accuracy on the test set at the end.
+# Make sure to print out your accuracy on the test set at the end.
 print("Final accuracy on test set:")
-print(sess.run(accuracy, feed_dict={X: mnist.test.images, y: mnist.test.labels}))
+print(sess.run(accuracy[0], feed_dict={X[0]: mnist.test.images, y[0]: mnist.test.labels}))
