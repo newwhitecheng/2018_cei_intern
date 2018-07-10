3b2a64f0c9eb93e5c1d29e04501f39d6923cb1bd
patricks-lab <patricks-lab@users.noreply.github.com>
2018-07-10 12:25:38 -0700

    Correct, faster version of decentralized model
    Note: you can experiment with for nei in [(cnode+1) % sz, cnode, (sz + cnode - 1) % sz]: or for nei in range(sz): to either select a "ring" decentralized network (N connections), or a dense graph where all nodes' gradients interact with each other (N(N-1))/2 connections. Next step: add noise, try to see if adversary can hack other gradients to determine input, try on bigger dataset (i.e. CIFAR or Cityscape, etc.).

--- privacy_preserving/decentralized_toy_network.py
+++ privacy_preserving/decentralized_toy_network.py
@@ -21,7 +21,7 @@ tf.reset_default_graph()
 theta_D = 1 #download rate
 theta_U = 0.01 #Upload rate
 
-sz = 4 # size of network
+sz = 10 # size of network
 
 # Variables for our  4- way network
 W1 = [tf.Variable(tf.truncated_normal([784, 500], stddev=0.1)) for i in range(sz)]
@@ -71,17 +71,20 @@ for i in range(sz):
 
 # Gradient descent for 4 channels
 train_one_step = [None for i in range(sz)]
-for i in range(sz):
-    train_one_step[i] = tf.train.GradientDescentOptimizer(0.05).minimize(loss[i])
+opt =[None for i in range(sz)]
 
-opt = tf.train.GradientDescentOptimizer(0.05)
+for i in range(sz):
+    opt[i] = tf.train.GradientDescentOptimizer(0.05)
+    train_one_step[i] = opt[i].minimize(loss[i])
+
+
 
 correct_predictions = [0.0 for i in range(sz)]
 accuracy = [0.0 for i in range(sz)]
 
 # Accuracy indicator for 4 channels
 for i in range(sz):
-    correct_predictions[i] = tf.equal(tf.argmax(scores[i], axis=1), tf.argmax(y[i], axis=1)) #something wrong with the axis dimension :-( !!
+    correct_predictions[i] = tf.equal(tf.argmax(scores[i], axis=1), tf.argmax(y[i], axis=1))
     accuracy[i] = tf.reduce_mean(tf.cast(correct_predictions[i], tf.float32))
 
 # Initialization of all decleared Tensors (above) in Tensorflow
@@ -96,50 +99,48 @@ sess.run(init) #initalize all variables
 train_acc=[0.0 for i in range(sz)]
 train_loss=[0.0 for i in range(sz)]
 
+send_gradients = [[None for i in range(sz)] for i in range(sz)]
+
+#-----------------Build the gradients transfer graph---------------------
+for cnode in range(sz):
+    #compute local gradients
+    g_W1[cnode], g_W2[cnode], g_W3[cnode], g_b1[cnode], g_b2[cnode], g_b3[cnode] = opt[cnode].compute_gradients(
+            loss=loss[cnode], var_list=[W1[cnode], W2[cnode], W3[cnode], b1[cnode], b2[cnode], b3[cnode]])
+    
+    #EXperimennt around with either 1)dense, N(N-1)/2 connections, or 2)sparse graph, N connections
+    #for nei in range(sz): #Dense graph
+    for nei in [(cnode+1) % sz, cnode, (sz + cnode - 1) % sz]:
+        #Distribute local gradients from current node to neighboring nodes
+        grad_W1[cnode][nei] = swapIdentity(g_W1[cnode], W1[nei])
+        grad_W2[cnode][nei] = swapIdentity(g_W2[cnode], W2[nei])
+        grad_W3[cnode][nei] = swapIdentity(g_W3[cnode], W3[nei])
+        grad_b1[cnode][nei] = swapIdentity(g_b1[cnode], b1[nei])
+        grad_b2[cnode][nei] = swapIdentity(g_b2[cnode], b2[nei])
+        grad_b3[cnode][nei] = swapIdentity(g_b3[cnode], b3[nei])
+        
+        send_gradients[cnode][nei] = opt[cnode].apply_gradients([
+                                 grad_W1[cnode][nei], grad_W2[cnode][nei], grad_W3[cnode][nei], 
+                                 grad_b1[cnode][nei], grad_b2[cnode][nei], grad_b3[cnode][nei]])
+
 #------------------------------Begin Training------------------------------
 
-#Split data into 4-ths. (750,750,750,750)
+#Split data into 4-ths. (50,50,50,50)
 
-for i in range(3000 // sz):
+for i in range(1000 // sz):
     
-    for cnode in range(sz):
- 
-        #----------download neighboring nodes' gradients (in this case, all (sz choose 2) or sz(sz-1)/2 nodes )------------
-        for nei in range(sz):
-            #Don't double train same node
-            if(nei == cnode):
-                continue
-
-            #Skip during 1st run of code
-            if(g_W1[nei] == None): 
-                continue
-
-            grad_W1[nei][cnode] = swapIdentity(g_W1[nei], W1[cnode])
-            grad_W2[nei][cnode] = swapIdentity(g_W2[nei], W2[cnode])
-            grad_W3[nei][cnode] = swapIdentity(g_W3[nei], W3[cnode])
-            grad_b1[nei][cnode] = swapIdentity(g_b1[nei], b1[cnode])
-            grad_b2[nei][cnode] = swapIdentity(g_b2[nei], b2[cnode])
-            grad_b3[nei][cnode] = swapIdentity(g_b3[nei], b3[cnode])
-            
-            opt.apply_gradients([grad_W1[nei][cnode], grad_W2[nei][cnode], grad_W3[nei][cnode], 
-                                 grad_b1[nei][cnode], grad_b2[nei][cnode], grad_b3[nei][cnode]])
-        #--------------------------------------------------------------
-        
-        
+    for cnode in range(sz):      
         #---------download minibatch of 50 & get gradients-----------------------
         X_batch, y_batch = mnist.train.next_batch(50)
         
-        if(i % 100 == 0):
+        #send gradients computed from current node to neighbors
+        for nei in [(cnode+1) % sz, cnode, (sz + cnode - 1) % sz]:
+        #for nei in range(sz):
+            sess.run(send_gradients[cnode][nei], feed_dict={X[cnode]: X_batch, y[cnode]: y_batch})
+        
+        if(i % 10 == 0):
             train_acc[cnode] = sess.run(accuracy[cnode], feed_dict={X[cnode]: X_batch, y[cnode]: y_batch})
             print("accuracy for Node: ", cnode,  "  is: ",train_acc[cnode],  "  at epoch: ", i)
-
-        #upload trained weights to neighbors B, C
-        g_W1[cnode], g_W2[cnode], g_W3[cnode], g_b1[cnode], g_b2[cnode], g_b3[cnode] = opt.compute_gradients(loss=loss[cnode], 
-                                                                      var_list=[W1[cnode], W2[cnode], W3[cnode],
-                                                                                b1[cnode], b2[cnode], b3[cnode]])
         
-        sess.run(train_one_step[cnode], feed_dict={X[cnode]: X_batch, y[cnode]: y_batch})
-        #---------------------------------------------------------------------
 
 # Make sure to print out your accuracy on the test set at the end.
 print("Final accuracy on test set:")
